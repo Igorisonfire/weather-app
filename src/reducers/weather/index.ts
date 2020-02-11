381b466e4d9018f8c81a1aea18d8faf063cc8848
igor_burak <igor.burak@zenio.co>
2020-02-11 13:36:29 +0300

    create new array

--- src/reducers/weather/index.ts
+++ src/reducers/weather/index.ts
@@ -10,6 +10,33 @@ const weatherReducer = (state = initState, action: any) => {
     switch (action.type) {
         case GET_WEATHER: {
 
+            const dateToDay = (date: string) => {
+                return Number(moment(date).format('D'))
+            }
+
+            let daysObj: any = {}
+
+            action.payload.list.reduce((prev: IWeather.WeatherSegment[], item: IWeather.WeatherSegment) => {
+
+                const filtered = prev.filter((itemPrev: any) => dateToDay(itemPrev.dt_txt) === dateToDay(item.dt_txt));
+
+                if(filtered.length > 0 && !(dateToDay(item.dt_txt) in daysObj)) {
+                    daysObj[dateToDay(item.dt_txt)] = filtered.concat(item);
+
+                    return prev.filter((itemPrev: any) => dateToDay(itemPrev.dt_txt) !== dateToDay(item.dt_txt));
+                }
+
+                if(dateToDay(item.dt_txt) in daysObj) {
+                    daysObj[dateToDay(item.dt_txt)].push(item)
+
+                    return prev;
+                }
+
+                return prev.concat(item);
+            }, [])
+
+            console.log(Object.values(daysObj));
+
             return {
                 ...state,
                 weather: action.payload
