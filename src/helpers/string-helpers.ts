04e40a89800ee435e4e499c9bb19c6bac7bdff58
igor_burak <igor.burak@zenio.co>
2020-02-11 16:38:47 +0300

    create new array

--- src/helpers/string-helpers.ts
+++ src/helpers/string-helpers.ts
@@ -1,1 +1,8 @@
+import moment from 'moment'
+
 export const capitalizeString = (str: string) => str[0].toUpperCase() + str.slice(1).toLowerCase();
+
+
+export const dateToDay = (date: string) => {
+    return Number(moment(date).format('D'))
+}
--- src/reducers/weather/index.ts
+++ src/reducers/weather/index.ts
@@ -1,6 +1,6 @@
 import IWeather from "./model";
 import {GET_WEATHER} from "../../actions/types";
-import moment from 'moment'
+import {dateToDay} from '../../helpers/string-helpers'
 
 const initState: IWeather.ModelState = {
     weather: null
@@ -10,24 +10,24 @@ const weatherReducer = (state = initState, action: any) => {
     switch (action.type) {
         case GET_WEATHER: {
 
-            const dateToDay = (date: string) => {
-                return Number(moment(date).format('D'))
-            }
-
             let daysObj: any = {}
 
-            action.payload.list.reduce((prev: IWeather.WeatherSegment[], item: IWeather.WeatherSegment) => {
+            action.payload.list.reduce((prev: any[], item: any) => {
 
                 const filtered = prev.filter((itemPrev: any) => dateToDay(itemPrev.dt_txt) === dateToDay(item.dt_txt));
 
-                if(filtered.length > 0 && !(dateToDay(item.dt_txt) in daysObj)) {
-                    daysObj[dateToDay(item.dt_txt)] = filtered.concat(item);
+
+                if(!(dateToDay(item.dt_txt) in daysObj)) {
+                    daysObj[dateToDay(item.dt_txt)] = {
+                        day: item.dt_txt,
+                        segments: filtered.concat(item)
+                    };
 
                     return prev.filter((itemPrev: any) => dateToDay(itemPrev.dt_txt) !== dateToDay(item.dt_txt));
                 }
 
                 if(dateToDay(item.dt_txt) in daysObj) {
-                    daysObj[dateToDay(item.dt_txt)].push(item)
+                    daysObj[dateToDay(item.dt_txt)].segments.push(item)
 
                     return prev;
                 }
@@ -35,7 +35,35 @@ const weatherReducer = (state = initState, action: any) => {
                 return prev.concat(item);
             }, [])
 
-            console.log(Object.values(daysObj));
+            const nerArr = Object.values(daysObj)
+
+
+
+            const testB = nerArr.map((item: any) => {
+
+                let maxTemp = item.segments[0].main.temp
+                let minTemp = item.segments[0].main.temp
+
+                for (let i = 0; i < item.segments.length; i++) {
+
+                    if (item.segments[i].main.temp > maxTemp) {
+                        maxTemp = item.segments[i].main.temp;
+                    }
+
+                    if (item.segments[i].main.temp < minTemp) {
+                        minTemp = item.segments[i].main.temp;
+                    }
+                }
+
+                return{
+                    maxTemp,
+                    minTemp,
+                    ...item
+                }
+            })
+
+            console.log(nerArr);
+            console.log('testB',testB);
 
             return {
                 ...state,
